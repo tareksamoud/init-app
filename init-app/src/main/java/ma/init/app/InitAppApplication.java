81bb3ef8f955c2382da8d1c4db5533c3fffa44fd
tsamoud <tarek.samoud@mertins-consulting.de>
2024-09-04 14:20:54 +0200

    initial commit

---
+++ init-app/src/main/java/ma/init/app/InitAppApplication.java
@@ -0,0 +1,13 @@
+package ma.init.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitAppApplication.class, args);
+    }
+
+}
